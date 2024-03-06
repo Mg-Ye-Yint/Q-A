fa2c2f6d26852286cd3940ab755cbdb8b4745ecd
Mg-Ye-Yint <tai31238@gmail.com>
2024-03-06 17:31:37 +0700

    Initial Commit

--- src/assets/Pages/Home.tsx
+++ src/assets/Pages/Home.tsx
@@ -1,8 +1,14 @@
 import { Box, Typography } from "@mui/material"
+import { themeApp } from "../utils/Theme";
 
 const Home = () => {
     return (
-        <Box>
+        <Box sx={{
+            display: "flex", width: "100%", background: "black", justifyContent: "center", marginBottom: "100px",
+            [themeApp.breakpoints.down("lg")]: {
+                width: "100%", height: "auto"
+            }
+        }}>
             <Typography sx={{color: "#1C1C1C", fontSize: "32", fontWeight: "bold"}}>
                 Start With
             </Typography>
